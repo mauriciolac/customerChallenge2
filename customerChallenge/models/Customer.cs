21ce110c42fde133547ac3ddc4e6e999d0a11172
Maurício Savaris <mauriciolac@gmail.com>
2018-03-22 22:08:28 -0300

    FirstCommit

---
+++ customerChallenge/models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customerChallenge.models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+
+    }
+}
